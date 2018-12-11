8c8015949f60fff13cded9c75dd8c283a34acccb
Taylor Williams <taylordwilliams90@gmail.com>
2018-12-11 13:26:03 -0700

    up

--- assignments/lesson-10/scripts/wu-weatherSpringfield.js
+++ assignments/lesson-10/scripts/wu-weatherSpringfield.js
@@ -1,8 +1,8 @@
 var weatherConditions = new XMLHttpRequest();
 var weatherForecast = new XMLHttpRequest();
 
-weatherConditions.open('GET', 'https://api.wunderground.com/api/cc7606ac5fc21a84/conditions/q/OR/Springfield.json', true);
-weatherForecast.open('GET', 'https://api.wunderground.com/api/cc7606ac5fc21a84/forecast/q/OR/Springfield.json', true);
+weatherConditions.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=83263', true);
+weatherForecast.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=83263', true);
 
 weatherConditions.send();
 weatherForecast.send();
